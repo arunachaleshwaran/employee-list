0b1b6e67ec658509b89ce3b280b462d5598bd21a
arunachaleshwaran <arunachaleshwaran12168@gmail.com>
2024-02-26 18:43:31 +0530

    💻add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,23 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', ".eslintrc.cjs"],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  "plugins": ['react-refresh', 'react-hooks'],
+  "parserOptions": {
+    "ecmaVersion": 2022,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
+  "rules": {
+    "react/react-in-jsx-scope": "off"
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 4000
+    port: 4000,
+    strictPort: true
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({ fix: true, include: '**/*.+(js|jsx|ts|tsx)' })
+  ],
 })

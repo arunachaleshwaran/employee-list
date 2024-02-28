8a50bc8f18d13ef79b5daf3aac0b243d6616a6d2
arunachaleshwaran <arunachaleshwaran12168@gmail.com>
2024-02-28 18:39:27 +0530

    ✨ add primary and secondary colors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -84,6 +84,7 @@ module.exports = {
     'no-nested-ternary': 'off',
     'no-ternary': 'off',
     'no-duplicate-imports': 'off',
+    'max-lines-per-function': 'off',
     'prettier/prettier': 'error',
   },
 };
